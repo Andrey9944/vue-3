dbe55bbebdfecd44cf8d8df04ee10673079a4072
andrey9944 <andriyseleznev7@gmail.com>
2023-12-10 22:35:47 +0300

    header&footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="v-footer-container">
+        <span class="line">
+            <div>Условия</div>
+           <div>Конфиденциальность</div>
+           <div>Безопасность</div>
+           <div>Управление файлами cookie</div>
+           <div>Не разглашать мою личную информацию</div>
+          OTUS Vue.JS developer 
+        </span>
+    </div>
+  </template>
+<style>
+.v-footer-container {
+    background: aliceblue;
+    padding: 25px 0;
+}
+.line {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: nowrap;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="v-header-block">
+        <div class="container-wrapper">
+           
+      </div>
+    </div>
+</template>
+
+<style>
+.v-header-block {
+    background: aliceblue;
+    height: 63px;
+    position: relative;
+    bottom: 60px;
+}
+.container-wrapper {
+    width: 100%;
+    padding: 30px 0;
+    border-bottom: 1px solid black;
+  }
+</style>
